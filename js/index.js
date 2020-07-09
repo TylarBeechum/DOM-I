5700ee68afabf2af0826a42d63ce07b7bb77f391
Tylar Washington-Beechum <tylay47@gmail.com>
2020-07-08 21:57:04 -0400

    7/8/2020

--- js/index.js
+++ js/index.js
@@ -40,3 +40,67 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelectorAll('a');
+const navs = document.querySelectorAll('a');
+const cta = document.getElementsByTagName('h1');
+const button = document.getElementsByTagName('button')
+const h4 = document.getElementsByTagName('h4');
+const para = document.getElementsByTagName('p');
+const foot = document.getElementsByTagName("footer")
+const img1 = document.querySelector("#cta-img");
+const img2 = document.querySelector(".middle-img");
+
+nav[0].textContent = "Services";
+nav[1].textContent = "Product";
+nav[2].textContent = "Vision";
+nav[3].textContent = "Features";
+nav[4].textContent = "About";
+nav[5].textContent = "Contact";
+
+navs[0].style.color = "green";
+navs[1].style.color = "green";
+navs[2].style.color = "green";
+navs[3].style.color = "green";
+navs[4].style.color = "green";
+navs[5].style.color = "green";
+
+
+cta[0].textContent = "DOM Is Awesome";
+
+button[0].textContent = "Get Started";
+
+h4[0].textContent = "Features";
+para[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."; 
+h4[1].textContent = "About";
+para[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+h4[2].textContent = "Services";
+para[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+h4[3].textContent = "Product";
+para[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+h4[4].textContent = "Vison";
+para[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+h4[5].textContent = "Contact";
+para[5].textContent = "123 Way 456 Street Somewhere, USA";
+para[6].textContent = "1 (888) 888-8888";
+para[7].textContent = "sales@greatidea.io";
+
+foot[0].textContent = "Copyright Great Idea! 2018"
+
+img1.src= "img/header-img.png";
+img2.src = "img/mid-page-accent.jpg";
+
+const head = document.getElementsByTagName('header');
+const subheadline = document.createElement('h1');
+subheadline.textContent = "My name is Tylar!";
+subheadline.style.color = "Purple";
+subheadline.style.fontSize ="3.4rem";
+head[0].prepend(subheadline);
+
+
+const hh = document.createElement('h1');
+hh.textContent = "I guess";
+hh.style.color = "slategrey";
+hh.style.fontSize = " 3.4rem";
+cta[1].prepend(hh);
